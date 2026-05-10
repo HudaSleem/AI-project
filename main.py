dcd69dd212c3f264b5e8ffba7d0ea078fa675479
HudaSleem <hodasleem4@gmail.com>
2026-05-10 20:11:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
 
+print("Running Loading...")
+os.system("jupyter nbconvert --to notebook --execute Loading.ipynb")
+
+print("Running Preprocessing...")
+os.system("jupyter nbconvert --to notebook --execute Preprocessing.ipynb")
+
+print("Running Training...")
+os.system("jupyter nbconvert --to notebook --execute Training.ipynb")
+
+print("Running Evaluation...")
+os.system("jupyter nbconvert --to notebook --execute Evaluation.ipynb")
+
+print("Done")
